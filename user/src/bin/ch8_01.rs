b6704ffbb51ca6fe2211383985583abcd116d183
zhanghx <731931282@qq.com>
2021-04-30 15:15:45 +0800

    将ch8_01修改回来

--- user/src/bin/ch8_01.rs
+++ user/src/bin/ch8_01.rs
@@ -1,7 +1,6 @@
 #![no_std]
 #![no_main]
 
-#[macro_use]
 extern crate user_lib;
 
 use user_lib::fork;
@@ -11,11 +10,7 @@ const NUM: usize = 10;
 #[no_mangle]
 pub fn main() -> i32 {
     for _ in 0..NUM {
-        let pid = fork();
-        // 应该保证有 initproc 回收 zombie process
-        if pid > 0 {
-            println!("forked new process {}", pid);
-        }
+        fork();
     }
     0
 }
