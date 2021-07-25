db1df174ef55aa9c7024a2c9d54474a1edde4be6
lrs <916583011@qq.com>
2021-07-25 20:12:44 +0800

    fix stack_string bug in ch2_write1.rs

--- user/src/bin/ch2_write1.rs
+++ user/src/bin/ch2_write1.rs
@@ -20,7 +20,18 @@ pub fn main() -> i32 {
         DATA_STRING.len() as isize
     );
     assert_eq!(write(STDOUT, &DATA_STRING.as_bytes()[..5]), 5);
-    let stack_string = "string from stack section\n";
+    // let stack_string = "string from stack section\n";
+    let string = "string from stack section!!\n";
+    let stack_string_holder: [u8; 50] = [0; 50];
+    let stack_string_u8 = unsafe {
+        core::ptr::copy(
+            string.as_ptr() as *const u8,
+            stack_string_holder.as_ptr() as *mut u8,
+            string.len(),
+        );
+        core::slice::from_raw_parts(stack_string_holder.as_ptr(), string.len())
+    };
+    let stack_string = core::str::from_utf8(stack_string_u8.as_ref()).unwrap();
     assert_eq!(
         write(STDOUT, stack_string.as_bytes()),
         stack_string.len() as isize
